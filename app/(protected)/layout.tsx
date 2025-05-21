ed72caab63c674a65e0a3e126dec4bd1296eb35d
Harsimran-Nugen <harsimran.nugen@gmail.com>
2025-05-21 17:29:47 +0530

    fix: added padding

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -20,7 +20,7 @@ export default async function ProtectedLayout({
       <Topbar />
       <div className="flex flex-1">
         <Sidebar />
-        <main className="flex-1">{children}</main>
+        <main className="flex-1 p-4">{children}</main>
       </div>
     </div>
   );
