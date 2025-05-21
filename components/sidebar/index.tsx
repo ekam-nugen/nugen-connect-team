a310fbfa7761647d2c408257ba517a174225429b
Harsimran-Nugen <harsimran.nugen@gmail.com>
2025-05-21 12:19:12 +0530

    chore: sidebar collapsable  functionality

--- components/sidebar/index.tsx
+++ components/sidebar/index.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import { sideBarData } from './constant';
 import { CgMenuGridO } from 'react-icons/cg';
-import { IoAddOutline } from 'react-icons/io5';
+import {
+  IoAddOutline,
+  IoChevronBackSharp,
+  IoChevronForwardSharp,
+} from 'react-icons/io5';
 import { BsThreeDotsVertical } from 'react-icons/bs';
-
 import { cn } from '@/lib/utils';
+import { Button } from '@/base-components/button';
 
 export default function Sidebar() {
+  const [collapsed, setCollapsed] = useState<boolean>(false);
+
   return (
-    <div className="w-56 bg-background space-y-4 p-2">
+    <div
+      className={cn(
+        'h-screen bg-background  transition-all duration-300 ease-in-out border',
+        collapsed ? 'w-16' : 'w-56 space-y-4 '
+      )}
+    >
+      <div className="w-full flex justify-end items-center">
+        <Button
+          onClick={() => setCollapsed(!collapsed)}
+          className="!w-1 !h-5 bg-gray-200 rounded-r-sm hover:none text-black"
+        >
+          {collapsed ? <IoChevronForwardSharp /> : <IoChevronBackSharp />}
+        </Button>
+      </div>
       {sideBarData.map((section, index) => (
-        <div key={section.title + index}>
-          {section?.section && (
+        <div key={section.title + index} className="p-1">
+          {!collapsed && section?.section && (
             <h2 className="text-sm font-bold mb-2 pl-2">{section.section}</h2>
           )}
           <ul className="space-y-1">
@@ -24,25 +44,32 @@ export default function Sidebar() {
                     'duration-500 hover:translate-x-1 transition-transform '
                 )}
               >
-                {section.hoverAnimate !== false && (
+                {section.hoverAnimate !== false && !collapsed && (
                   <CgMenuGridO className="h-6 w-6 text-gray-500 hidden group-hover:flex transition-opacity group-hover:duration-500" />
                 )}
                 <span
-                  className={`text-xl ${section?.section ? 'text-background' : 'text-foreground'} flex items-center justify-center rounded-md ${item?.color} px-1 p-1`}
+                  className={cn(
+                    'text-xl flex items-center justify-center rounded-md px-1 p-1',
+                    section?.section ? 'text-background' : 'text-foreground',
+                    item?.color,
+                    collapsed ? 'mx-auto' : 'px-1'
+                  )}
                 >
                   <item.icon />
                 </span>
-                <div className="flex justify-between w-full">
-                  <div className="text-sm cursor-pointer">{item.title}</div>
-                  {section.hoverAnimate !== false && (
-                    <BsThreeDotsVertical className="h-4 w-4 mr-0.5 text-gray-500 hidden group-hover:flex transition-opacity group-hover:duration-500" />
-                  )}
-                </div>
+                {!collapsed && (
+                  <div className="flex justify-between w-full">
+                    <div className="text-sm cursor-pointer">{item.title}</div>
+                    {section.hoverAnimate !== false && (
+                      <BsThreeDotsVertical className="h-4 w-4 mr-0.5 text-gray-500 hidden group-hover:flex transition-opacity group-hover:duration-500" />
+                    )}
+                  </div>
+                )}
               </div>
             ))}
           </ul>
 
-          {section.addOption && (
+          {section.addOption && !collapsed && (
             <div className="my-2 pl-2 flex items-center gap-2">
               <span
                 className={`text-xl w-6 h-6 flex text-blue-400 items-center justify-center rounded-full border-[1px] border-gray-400 px-1 p-1`}
