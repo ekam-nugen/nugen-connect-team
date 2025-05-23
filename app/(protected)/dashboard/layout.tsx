863e64d137bbc843689a9ec4e85bc0a94270a1f4
harmanthind001 <harmannugen@gmail.com>
2025-05-23 12:53:11 +0530

    remove extra pages

--- app/(protected)/dashboard/layout.tsx
+++ app/(protected)/dashboard/layout.tsx
@@ -3,19 +3,13 @@ import { ReactNode } from 'react';
 export default function DashboardLayout({
   children,
   dashboard,
-  // profile,
-  // user,
 }: {
   children: ReactNode;
   dashboard: ReactNode;
-  // profile: ReactNode;
-  // user: ReactNode;
 }) {
   return (
     <div className="grid gap-4">
       <section>{dashboard}</section>
-      {/* <section className="bg-background shadow p-4">{profile}</section> */}
-      {/* <section className="bg-background shadow p-4">{user}</section> */}
       <div className="col-span-2">{children}</div>
     </div>
   );
